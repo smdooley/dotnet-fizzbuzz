d9c1aeae910f3b87a44655aeed2eaef070452a21
smdooley <sean.dooley.22@gmail.com>
2025-09-18 09:23:53 +0000

    Refactor FizzBuzz implementation to improve code structure and readability

--- Program.cs
+++ Program.cs
@@ -2,8 +2,11 @@
 Console.WriteLine("Enter a number:");
 int number = Convert.ToInt32(Console.ReadLine());
 
+// Approach 1: If/Else
+Console.WriteLine("Approach 1: If/Else");
 for (int i = 1; i <= number; i++)
 {
+    // Approach 1:
     if (i % 3 == 0 && i % 5 == 0)
     {
         Console.WriteLine("FizzBuzz");
@@ -20,4 +23,18 @@ for (int i = 1; i <= number; i++)
     {
         Console.WriteLine(i);
     }
+}
+
+// Approach 2: String Concatenation
+Console.WriteLine("Approach 2: String Concatenation");
+
+for (int i = 1; i <= number; i++)
+{
+    string output = "";
+
+    if (i % 3 == 0) output += "Fizz";
+    if (i % 5 == 0) output += "Buzz";
+    if (output == "") output = i.ToString();
+
+    Console.WriteLine(output);
 }
